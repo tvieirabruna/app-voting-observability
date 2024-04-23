c8b991c8011be8a2618101ae47c18f27f94dc0f2
Bruna <brunavieira@nano.ufrj.br>
2024-04-22 21:56:47 -0300

    Add backend.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,3 @@
-# Configure Terraform to use an S3 backend
-terraform {
-  backend "s3" {
-    bucket = "app-voting-terraform-state-bucket"  # Unique S3 bucket for state storage
-    key    = "app-voting/terraform.tfstate"  # Path to store the state in the bucket
-    region = "us-east-1"  # AWS region where the S3 bucket is located
-  }
-}
-
 # Define AWS provider and region
 provider "aws" {
   region = "us-east-1"  # Change to your preferred region
