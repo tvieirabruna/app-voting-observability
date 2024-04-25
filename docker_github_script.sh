ea743cf666be19987d139148a47821461cf1e73c
Bruna <brunavieira@nano.ufrj.br>
2024-04-25 01:35:01 -0300

    fix: fix Docker installation setup

--- docker_github_script.sh
+++ docker_github_script.sh
@@ -3,15 +3,27 @@ sudo apt-get update
 
 # Install Docker in Ubuntu
 # Add Docker's official GPG key:
-sudo apt install apt-transport-https ca-certificates curl software-properties-common -Y
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt update
-apt-cache policy docker-ce
-sudo apt install docker-ce -Y
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+# Install Docker packages
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 
 # Install Docker Compose
-sudo apt install docker-compose -Y
+sudo apt-get install docker-compose-plugin -y
+
+# Upgrade and update packages
+sudo apt-get upgrade
+sudo apt-get update
 
 # Clone the GitHub repository
 sudo apt-get install -y git
